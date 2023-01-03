02e50f9c7ca622d9665a7fe457b2aab0788b625a
Hung Vu <vunamhung@gmail.com>
2023-01-03 09:02:49 +0700

    :recycle: Refactoring code.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,6 @@ export default class CustomDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/icons/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" />
         </Head>
         <body>
           <Main />
